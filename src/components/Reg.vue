5274be48ce67f5c4eb2d27e7cbdf32c813487f89
Rob Donnelly <Robert.Donnelly@jpl.nasa.gov>
2021-10-27 10:38:05 -0700

    feat: add element doc

--- src/components/Reg.vue
+++ src/components/Reg.vue
@@ -1,5 +1,6 @@
 <template>
   <RegLayout :reg="reg"></RegLayout>
+  <div id="doc"><span v-html='doc'></span></div>
   <RegFields :reg="reg"></RegFields>
 </template>
 
@@ -10,3 +11,27 @@ defineProps([
   'reg'
 ]);
 </script>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    doc() {
+      // The doc key may not be present
+      if (this.reg.doc) {
+        return this.reg.doc.replaceAll("\n", "<br>");
+      } else {
+        return "";
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+#doc {
+  text-align: left;
+}
+</style>
