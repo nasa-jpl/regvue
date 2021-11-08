f84c5a8af554b48aae01b40d274eeee9764a96de
Rob Donnelly <Robert.Donnelly@jpl.nasa.gov>
2021-11-08 11:29:35 -0800

    style

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-// Enable running w/o a webserver
-//
-// References:
-// * https://stackoverflow.com/a/53337969
-// * https://cli.vuejs.org/config/#vue-config-js
 module.exports = {
-    publicPath: '',
+  // Enable running w/o a webserver
+  //
+  // References:
+  // * https://stackoverflow.com/a/53337969
+  // * https://cli.vuejs.org/config/#vue-config-js
+  publicPath: ''
 }
