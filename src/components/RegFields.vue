ee0095d3f367bcfd5d83c8e217d83327c44c955a
Rob Donnelly <Robert.Donnelly@jpl.nasa.gov>
2022-02-17 14:10:17 -0800

    fix(warning): defineProps doesn't need to be imported
     [@vue/compiler-sfc] `defineProps` is a compiler macro and no longer
     needs to be imported.
    
    See https://eslint.vuejs.org/user-guide/#compiler-macros-such-as-defineprops-and-defineemits-generate-no-undef-warnings

--- src/components/RegFields.vue
+++ src/components/RegFields.vue
@@ -20,8 +20,6 @@
 </template>
 
 <script setup>
-import { defineProps } from 'vue'
-
 defineProps([
   'reg',
   'field_name',
--- src/components/RegLayout.vue
+++ src/components/RegLayout.vue
@@ -30,8 +30,6 @@
 </template>
 
 <script setup>
-import { defineProps } from 'vue'
-
 defineProps([
   'reg',
   'field_name',
