517d94ebc171cb42107d5a6a3d86911d2be585f4
Rob Donnelly <Robert.Donnelly@jpl.nasa.gov>
2022-02-18 15:32:55 -0800

    update app to match schema v0.2
    NOTE: Need to use BigInt for values that exceed 32 bits.  For example,
    when calculating the field mask, we do 1 << field.nbits.  For a 32 bit
    field, this will overflow and become 0.  To fix, the expression needs to
    become 1n << BigInt(field.nbits).

---
+++ app/src/parse.js
@@ -0,0 +1,39 @@
+// Provides various string parsing functions
+export default {
+  // Parses numeric values in various formats
+  //
+  // Supports various bases.  See base().
+  //
+  // Supports '_' separators.
+  num(s) {
+    let {str, base} = this.base(s.trim())
+    return parseInt(str.replaceAll('_', ''), base)
+  },
+  // Parses the base of a numeric value
+  //
+  // Supports:
+  //
+  // * Base 2 - '0b' prefix
+  // * Base 10 - no prefix
+  // * Base 16 - '0x' prefix
+  base(s) {
+    let lc = s.toLowerCase()
+
+    if (lc.startsWith("0x")) {
+      return {
+        str: lc.slice(2),
+        base: 16,
+      }
+    } else if (lc.startsWith("0b")) {
+      return {
+        str: lc.slice(2),
+        base: 2,
+      }
+    } else {
+      return {
+        str: lc,
+        base: 10,
+      }
+    }
+  },
+}
