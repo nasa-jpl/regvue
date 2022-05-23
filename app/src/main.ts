1bfdf741efb48d8cb44b7617e988d8fc1a9f3650
Josh Geden <Joshua.M.Geden@jpl.nasa.gov>
2022-05-23 14:37:18 -0700

    remove devtools (currently unused)

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,11 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import devtools from "@vue/devtools";
 import "./index.css";
-
-if (process.env.NODE_ENV === "development") {
-  devtools.connect();
-}
 
 createApp(App).use(router).mount("#app");
