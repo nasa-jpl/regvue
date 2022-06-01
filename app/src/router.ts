aa25544b0b718fc000ec6159e3f83d99dd49e598
Josh Geden <Joshua.M.Geden@jpl.nasa.gov>
2022-06-01 14:12:24 +0000

    use createWebHashHistory instead of createWebHistory

--- app/src/router.ts
+++ app/src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Default from "./views/Default.vue";
 import RegView from "./views/RegView.vue";
 
@@ -23,7 +23,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
