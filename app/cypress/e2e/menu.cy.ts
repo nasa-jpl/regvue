d6a84f5dbb77a4e11cf607d25ff94cb5ba88928d
Josh Geden <Joshua.M.Geden@jpl.nasa.gov>
2022-06-14 15:58:45 +0000

    update Menu test

--- app/cypress/e2e/menu.cy.ts
+++ app/cypress/e2e/menu.cy.ts
@@ -5,13 +5,20 @@ import data from "../../public/data.json";
 
 describe("menu-component", () => {
   beforeEach(() => {
-    cy.visit("localhost:3000");
+    cy.visit("/");
   });
 
   it("shows the unselected root nodes", () => {
-    cy.get("[id^=menu-node-]").should("have.length", data.root.children.length);
+    const rootElements = data.root.children;
+    const subBlks = data.elements[rootElements[0]].children;
+    const regs = data.elements[subBlks[0]].children;
+
+    cy.get("[id^=menu-node-]").should(
+      "have.length",
+      rootElements.length + subBlks.length + regs.length
+    );
     data.root.children.forEach((child) => {
-      const id = "#menu-node-" + child.replace(".", "-");
+      const id = "#menu-node-" + child.split(".").join("-");
 
       // Shows the name of the menu node
       cy.get(id).children().should("have.length", 2);
@@ -27,37 +34,33 @@ describe("menu-component", () => {
   });
 
   it("highlights only one menu item", () => {
-    const id0 = "#menu-node-" + data.root.children[0].replace(".", "-");
+    const id0 = "#menu-node-" + data.root.children[0].split(".").join("-");
     cy.get(id0).click().should("have.class", "bg-blue-200");
 
     data.root.children.slice(1).forEach((child) => {
-      const id = "#menu-node-" + child.replace(".", "-");
+      const id = "#menu-node-" + child.split(".").join("-");
       cy.get(id).should("not.have.class", "bg-blue-200");
     });
 
-    const id1 = "#menu-node-" + data.root.children[1].replace(".", "-");
+    const id1 = "#menu-node-" + data.root.children[1].split(".").join("-");
     cy.get(id1).click().should("have.class", "bg-blue-200");
     cy.get(id0).should("not.have.class", "bg-blue-200");
   });
 
   it("toggles children visiblity on click", () => {
-    const parent = data.root.children[0];
-    cy.get("[id^=menu-node-]").should("have.length", data.root.children.length);
+    const rootElements = data.root.children;
+    const subBlks = data.elements[rootElements[0]].children;
+    const regs = data.elements[subBlks[0]].children;
 
-    cy.get(".open-menu-node-btn").first().click();
     cy.get("[id^=menu-node-]").should(
       "have.length",
-      data.root.children.length + data.elements[parent].children.length
+      rootElements.length + subBlks.length + regs.length
     );
 
-    data.elements[parent].children.forEach((child: string) => {
-      const id = "#menu-node-" + child.replace(".", "-");
+    data.elements[subBlks[0]].children.forEach((child: string) => {
+      const id = "#menu-node-" + child.split(".").join("-");
 
-      cy.get(id)
-        .should("not.have.class", "bg-blue-200")
-        .children()
-        .first()
-        .and("have.text", child.split(".").pop());
+      cy.get(id).children().first().and("have.text", child.split(".").pop());
 
       cy.get(id)
         .children()
@@ -66,12 +69,18 @@ describe("menu-component", () => {
     });
 
     cy.get(".close-menu-node-btn").first().click();
-    cy.get("[id^=menu-node-]").should("have.length", data.root.children.length);
+    cy.get("[id^=menu-node-]").should("have.length", rootElements.length);
+
+    cy.get(".open-menu-node-btn").first().click();
+    cy.get("[id^=menu-node-]").should(
+      "have.length",
+      rootElements.length + subBlks.length
+    );
   });
 
   it("changes route on menu node click", () => {
     const name = data.root.children[0];
-    const id = "#menu-node-" + name.replace(".", "-");
+    const id = "#menu-node-" + name.split(".").join("-");
 
     cy.get(id).click();
     cy.url().should("include", `/reg/${name}`);
@@ -91,5 +100,11 @@ describe("menu-component", () => {
         data.elements[firstParent].children.length +
         data.elements[firstChild].children.length
     );
+  });
+
+  it("can be closed and reopened", () => {
+    cy.get("#navigation-menu").should("be.visible");
+    cy.get("#toggle-menu-button").click();
+    cy.get("#navigation-menu").should("not.be.visible");
   });
 });
