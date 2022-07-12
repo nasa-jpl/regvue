a60dc5a3bf6ec772aca8e2812a611fc2eea49007
Josh Geden <Joshua.M.Geden@jpl.nasa.gov>
2022-07-12 16:52:10 +0000

    style enum values like a dropdown menu

--- app/src/components/FieldInputBox.vue
+++ app/src/components/FieldInputBox.vue
@@ -4,6 +4,9 @@ import { Bit, DisplayType, isUnknownBit } from "src/types";
 import format from "src/format";
 import parse from "src/parse";
 
+import ChevronDown from "vue-material-design-icons/ChevronDown.vue";
+import ChevronUp from "vue-material-design-icons/ChevronUp.vue";
+
 const props = defineProps<{
   name: string;
   bitArray: Bit[];
@@ -44,6 +47,13 @@ let showEnum = ref(false);
 
 // Determine whether or not to show the error tooltip below the input
 let showErrorTooltip = ref(false);
+
+// Focuses on the input box
+const focusOnInput = () => {
+  (
+    document.querySelector(`#input-box-${props.name}`) as HTMLInputElement
+  ).focus();
+};
 
 // Remove the highlight, deselect the input, and reformat the
 // displayValue if there is no error
@@ -209,6 +219,17 @@ const getErrorMessage = (value: string) => {
         @mouseenter="showErrorTooltip = true"
         @mouseleave="showErrorTooltip = false"
       />
+
+      <chevron-down
+        v-if="enums.length && !showEnum"
+        class="z-20 col-start-1 row-start-1 justify-self-end hover:cursor-pointer"
+        @click="focusOnInput()"
+      />
+      <chevron-up
+        v-else-if="enums.length && showEnum"
+        class="z-20 col-start-1 row-start-1 justify-self-end hover:cursor-pointer"
+        @blur="showEnum = false"
+      />
     </div>
 
     <!-- Show that the field value has an error -->
@@ -233,15 +254,10 @@ const getErrorMessage = (value: string) => {
     <!-- Show the enum value dropdown options -->
     <div
       v-else-if="showEnum && enums.length"
-      class="absolute top-[1.1rem] left-[50%] z-50 mt-1 w-fit translate-x-[-50%]"
+      class="absolute top-[calc(1.25rem+1px)] left-[50%] z-50 mt-1 w-fit translate-x-[-50%]"
     >
-      <!-- Display small triangle pointing up -->
       <div
-        class="m-auto h-0 w-0 border-[6px] border-transparent border-b-gray-400"
-      ></div>
-
-      <div
-        class="rounded border border-b-0 border-t-4 border-gray-400 border-t-gray-400 bg-gray-200"
+        class="rounded border border-b-0 border-gray-400 border-t-gray-400 bg-white"
       >
         <div
           v-for="e in enums"
